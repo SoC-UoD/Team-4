0c9a94dd562d6f4760831a53bddb4e7e465f1a1e
Toby <toby.philp@mac.com>
2012-11-07 16:05:16 +0000

    Added a basic container class for stock data with getters and setters.

--- com.example.test.MainActivity/src/com/example/test/Stock.java
+++ com.example.test.MainActivity/src/com/example/test/Stock.java
@@ -1,13 +1,71 @@
 package com.example.test;
 
 public class Stock {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private double _previousFridayClosePrice;
+	private String _previousFridayDate;
+	
+	private double _currentPrice;
+	private double _marketCap;
+	private int _volume;
+	private double _percentChange;
+	
+	public double getPreviousFriClosePrice()
+	{
+		return _previousFridayClosePrice;
+	}
+	
+	public void setPreviousFriClosePrice(double newPrice) 
+	{
+		_previousFridayClosePrice = newPrice;
+	}
+	
+	public double getCurrentPrice()
+	{
+		return _currentPrice;
+	}
+	
+	public void setCurrentPrice(double newPrice) 
+	{
+		_currentPrice = newPrice;
+	}
+	
+	public String getPreviousFriDate()
+	{
+		return _previousFridayDate;
+	}
+	
+	public void setPreviousFriDate(String newDate) 
+	{
+		_previousFridayDate = newDate;
+	}
+	
+	public void setMarketCap(double newCap)
+	{
+		_marketCap = newCap;
+	}
+	
+	public double getMarketCap()
+	{
+		return _marketCap;
+	}
+	
+	public void setVolume(int newVolume)
+	{
+		_volume = newVolume;
 	}
 
+	public int getVolume()
+	{
+		return _volume;
+	}
+	
+	public void setPercentChange(double newPercent)
+	{
+		_percentChange = newPercent;
+	}
+	
+	public double getPercentChange()
+	{
+		return _percentChange;
+	}
 }
