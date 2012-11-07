5ceeb5da118e94e7f5d11851ba48f25e7c895137
Ross Teviotdale <r.d.teviotdale@dundee.ac.uk>
2012-11-07 12:09:17 +0000

    Added test setup

---
+++ TDD/src/StockTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import com.example.test.Stock;
+
+public class StockTest {
+
+	@Test
+	public void test() {
+		assertTrue(Stock.abc);
+	}
+
+}
